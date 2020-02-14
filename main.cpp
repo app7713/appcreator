c00c027667cbe386ebe7c6bbbfa5b2b502ce4dd2
app7713 <58703899+app7713@users.noreply.github.com>
2020-02-14 21:23:21 +0000

    TEST WINDOWS HACK RF HAVOC COMPILED
    Ok guys right im thinking ive got havoc bin in linux compiled to windows but im getting two array errors off? test it please id be gratful if im right it compiled ok any ?s im willing move into a new beyond havoc piece cheers dave

---
+++ main.cpp
@@ -0,0 +1,3 @@
+{main, cpp - console} -{_single_inheritance }-__VARIADIC_TEMPLATES - { co_return } - declar - array - {delete} --co_return void -
+{ hackrf_spiflash;  portapack  h1  havoc.bin unix - [typedef]
+    return 0 -void,_IO_cookie_init-typedef
